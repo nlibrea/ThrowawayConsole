1d8c83fefc52a9626c7c3b08a3eb785e668712de
nlibrea <nlibrea@ymail.com>
2016-09-12 16:54:12 -0600

    Simple console app

---
+++ StringQuack/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringQuack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello World";
+            Console.WriteLine(message.Quack());
+        }
+    }
+
+    public static class StringExtensions
+    {
+        public static string Quack(this string self)
+        {
+            return self + " (quack)";
+        }
+    }
+
+}
